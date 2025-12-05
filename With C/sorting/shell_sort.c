335cc4b1c8c45fd502fbcd783a94e5311cf8fe82
Subhojyoti Maity <subhojyotimaity1082005@gmail.com>
2025-12-05 12:30:19 +0530

    Update shell_sort.c

--- With C/sorting/shell_sort.c
+++ With C/sorting/shell_sort.c
@@ -3,7 +3,8 @@ void shellSort(int arr[], int n){
     for (int gap = n / 2; gap > 0; gap /= 2)
         for (int i = gap; i < n; i++){
             int temp = arr[i], j;
-            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) arr[j] = arr[j - gap];
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
+              arr[j] = arr[j - gap];
             arr[j] = temp;
      }
 }
